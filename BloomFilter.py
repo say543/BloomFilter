be41df1a4a97d728913f928d1f4a20f99d3bf624
Chieh-Chun Chang <say543@msn.com>
2019-01-02 09:41:11 -0800

    Update BloomFilter.py

--- BloomFilter.py
+++ BloomFilter.py
@@ -17,7 +17,6 @@ class BloomFilter(object):
             items_count(int):  number of items expected to be stored in filter 
             fp_prob (float): False Positive probability in decimal, optional argument
             hash_count(int):  number of hash function cnt will be used
-
         """
         if items_count <= 0:
             raise ValueError("items_count must be bigger than 0")
@@ -27,7 +26,6 @@ class BloomFilter(object):
             raise ValueError("hash_count must be bigger than 0")
 
         if hash_count != None:
-
             # False posible probability in decimal 
             self.fp_prob = fp_prob 
 
@@ -40,7 +38,6 @@ class BloomFilter(object):
 
             # number of hash functions to use 
             self.hash_count = (int) (self.get_hash_count_by_fp_prob(fp_prob))
-
             self.size = (int)( self.get_size(items_count, fp_prob))
             
         # outout initlization information
@@ -86,7 +83,6 @@ class BloomFilter(object):
     def clear_all(self): 
         """
         clear all items in bloom filter
-
         """
         self.bit_array.setall(0)
   
@@ -94,13 +90,11 @@ class BloomFilter(object):
     def get_size(self, items_count, fp_prob): 
         """
         array_size = -(items_count * lg(fp_prob)) / (lg(2)^2) 
-
         Args:
             items_count(int):  number of items expected to be stored in filter 
             fp_prob (float): False Positive probability in decimal .
         Returns:
             float: Return the size of bit array(m) to used
-
         """
         if items_count == None or items_count <= 0:
             raise ValueError("items_count must be bigger than 0")
@@ -112,17 +106,10 @@ class BloomFilter(object):
     @classmethod
     def get_hash_count_by_array_size_and_element_size(self, array_size, items_count): 
         """
-
         hash_count = (array_size / items_count) * ln(2) 
-  
-        fp_prob : float 
-            False Positive probability in decimal 
-
         Args:
             array_size (int): size of bit array  wiil be used
             items_count(int):  number of items expected to be stored in filter 
-
-
         Returns:
             float: Return the optimized hash cnt to be used
         """
@@ -138,13 +125,8 @@ class BloomFilter(object):
     def get_hash_count_by_fp_prob(self, fp_prob): 
         """
         hash_count =  (-1) log2(desired probability) 
-  
-        fp_prob : float 
-            False Positive probability in decimal 
-
         Args:
             fp_prob (float): False Positive probability in decimal .
-
         Returns:
             float: Return the optimized hash cnt to be used based on fp_prob
         """
@@ -154,13 +136,10 @@ class BloomFilter(object):
         hash_count = (-1) * math.log(fp_prob, 2) 
         return hash_count 
 
-    
     @classmethod
     def get_size_by_hash_count_and_fp_prob(self, items_count, hash_count, fp_prob): 
         """
-
         array_size =  items_count * 2 * hash_count / (2* fp_prob^(1/hash_count) + fp_prob^(1/hash_count) * fp_prob^(1/hash_count))
-
         Args:
             items_count(int):  number of items expected to be stored in filter 
             hash_count(int):  number of hash function cnt will be used
@@ -169,7 +148,6 @@ class BloomFilter(object):
         Returns:
             float: return optimized array size to be used based on fp_prob and hash count
         """
-
         if items_count == None or items_count <= 0:
             raise ValueError("items_count must be bigger than 0")
         if fp_prob == None or fp_prob <= 0:
@@ -182,4 +160,3 @@ class BloomFilter(object):
         ratio = 2 * hash_count / (2* fp_prob_with_power + fp_prob_with_power * fp_prob_with_power)
         array_size = ratio * items_count
         return array_size
-   
